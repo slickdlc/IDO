8cd3583e39031565071c6728ab563269b5bbac87
Bnito28 <72296548@udaff.edu.pe>
2020-09-11 20:34:59 -0500

    nombre

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('frontoffice/nosotros');
+    return view('frontoffice/principal');
 });
 
 Auth::routes();
